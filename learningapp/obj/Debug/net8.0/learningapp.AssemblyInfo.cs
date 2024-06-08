baca1c62b3e7457f1f02b20bc3ce29ef97c06eb1
Abdelkrim MRISSE <mrisseabdelkrim@gmail.com>
2024-06-08 12:44:42 -0400

    second commit

--- learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
+++ learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c46bbcd8bb0bda30a569fd777546166331cf0d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e41600c33d2b82a74d13118749a42d8a7a613fd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
